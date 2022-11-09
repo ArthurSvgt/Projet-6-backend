b6aae25d1f6e78e69565341353b30643cc23c8ae
Arthur Souvignet <arthursouvignet@hotmail.fr>
2022-11-09 16:43:39 +0100

    Ajout du Signup fonctionnel sans hachage et ajout du UserModel

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('./models/user');
+
+mongoose.connect('mongodb+srv://asouvignet2:AxUllgqFMJLCk1GQ@cluster1.djkzh50.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 app.use(express.json());
@@ -12,10 +23,27 @@ app.use((req, res, next) => {
 
 app.post('/api/auth/signup', (req, res, next) => {
     console.log(req.body);
-    res.status(201).json({
-        message: 'SignUp réussi !'
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
     });
+    console.log(user);
+    user.save()
+        .then(
+            () => {
+                res.status(201).json({
+                    message: 'Utilisateur enregistré dans la base de données.'
+                });
+            }
+        ).catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
+            }
+        );
 });
+
 
 app.post('/api/auth/login', (req, res, next) => {
     console.log(req.body);
@@ -23,5 +51,6 @@ app.post('/api/auth/login', (req, res, next) => {
         message: 'LogIn réussi !'
     });
 });
+
 
 module.exports = app;
